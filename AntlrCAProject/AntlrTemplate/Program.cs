3a964cceec456045b45cfb7cc63839d52a7eb97e
Ken Domino <kenneth.domino@domemtech.com>
2019-12-06 15:19:15 -0500

    Adding in Net Core app template. Install using 'dotnet new --install Antlr4BuildTasks/AntlrCAProject'. To run, mkdir some-dir-name; cd !$; dotnet new antlr.

---
+++ AntlrCAProject/AntlrTemplate/Program.cs
@@ -0,0 +1,31 @@
+﻿using Antlr4.Runtime;
+
+namespace AntlrTemplate
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = "1 + 2 * 3";
+            var str = new AntlrInputStream(input);
+            var lexer = new arithmeticLexer(str);
+            var tokens = new CommonTokenStream(lexer);
+            var parser = new arithmeticParser(tokens);
+            var listener = new ErrorListener<IToken>();
+            parser.AddErrorListener(listener);
+            var tree = parser.file();
+            if (listener.had_error)
+            {
+                System.Console.WriteLine("error in parse.");
+            }
+            else
+            {
+                System.Console.WriteLine("parse completed.");
+                System.Console.WriteLine(tokens.OutputTokens());
+                System.Console.WriteLine(tree.OutputTree(tokens));
+            }
+            var visitor = new CalculatorVisitor();
+            visitor.Visit(tree);
+        }
+}
+}
