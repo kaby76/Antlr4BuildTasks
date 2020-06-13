6082afb5f73a4f2ef4f1f5c9820f8cd79c4c83bc
Ken Domino <kenneth.domino@domemtech.com>
2020-06-13 15:57:40 -0400

    Finally, this is starting to work. All possible paths tried with build, VS2019, Rider, VSCode. I don't like that I have to generate the files from the tool at design time, but I haven't figured out a way to kick off a design time build after a normal build.

---
+++ Antlr4BuildTasks/MakeRelativeList.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using Directory = System.IO.Directory;
+using Path = System.IO.Path;
+
+namespace Antlr4.Build.Tasks
+{
+    public class MakeRelativeList
+            : Task
+    {
+        public ITaskItem[] List1
+        {
+            get;
+            set;
+        }
+
+        private List<ITaskItem> _result = new List<ITaskItem>();
+
+        [Output]
+        public ITaskItem[] Result
+        {
+            get
+            {
+                return _result.ToArray();
+            }
+            set
+            {
+                _result = new List<ITaskItem>(value);
+            }
+        }
+
+        public override bool Execute()
+        {
+            _result = new List<ITaskItem>();
+            string current = Directory.GetCurrentDirectory();
+            foreach (var v2 in List1)
+            {
+                var f = v2.ToString();
+                var is_full_path = System.IO.Path.IsPathRooted(f);
+                if (!is_full_path)
+                {
+                    f = System.IO.Path.GetFullPath(f);
+                }
+                var absolute = f;
+                var relative_path = f.Replace(current,"");
+                if (relative_path[0] == '/' || relative_path[0] == '\\')
+                {
+                    relative_path = relative_path.Substring(1);
+                }
+                _result.Add(new TaskItem() { ItemSpec = relative_path });
+            }
+            return true;
+        }
+    }
+}
---
+++ Antlr4BuildTasks/SubtractList.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Directory = System.IO.Directory;
+using File = System.IO.File;
+using FileAttributes = System.IO.FileAttributes;
+using Path = System.IO.Path;
+
+namespace Antlr4.Build.Tasks
+{
+    public class SubtractList
+            : Task
+    {
+        public ITaskItem[] List1
+        {
+            get;
+            set;
+        }
+
+        public ITaskItem[] List2
+        {
+            get;
+            set;
+        }
+
+        private List<ITaskItem> _result = new List<ITaskItem>();
+
+        [Output]
+        public ITaskItem[] Result
+        {
+            get
+            {
+                return _result.ToArray();
+            }
+            set
+            {
+                _result = new List<ITaskItem>(value);
+            }
+        }
+
+
+        public override bool Execute()
+        {
+            _result = new List<ITaskItem>(List1);
+            foreach (var v2 in List2)
+            {
+                if (_result.Contains(v2))
+                    _result.Remove(v2);
+            }
+            return true;
+        }
+    }
+}
