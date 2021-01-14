8502e213a56187356ca95b4001cea4ce42e3488a
Ken Domino <kenneth.domino@domemtech.com>
2021-01-14 15:44:39 -0500

    Simplifying the old antlr dotnet template since dotnet-antlr antlr will be the new antlr generator program.

--- Templates/templates/AntlrCAProject/Program.cs
+++ Templates/templates/AntlrCAProject/Program.cs
@@ -9,37 +9,23 @@ namespace TemplateNamespace
         static bool have_files = false;
         static void Main(string[] args)
         {
-            have_files = args.Length > 0;
-            if (have_files)
-            {
-                for (int i = 0; i < args.Length; ++i)
-                {
-                    if (args[i].StartsWith("-")) continue;
-                    var fn = args[i];
-                    var input = ReadAllInput(fn);
-                    Try(input);
-                }
-            }
-            else
-            {
-                Try("1 + 2 + 3");
-                Try("1 2 + 3");
-                Try("1 + +");
-            }
+            Try("1 + 2 + 3");
+            Try("1 2 + 3");
+            Try("1 + +");
         }
 
         static void Try(string input)
         {
             var str = new AntlrInputStream(input);
             System.Console.WriteLine(input);
-            var lexer = new MyLexerGrammar(str);
+            var lexer = new ArithmeticLexer(str);
             var tokens = new CommonTokenStream(lexer);
-            var parser = new MyParserGrammar(tokens);
+            var parser = new ArithmeticParser(tokens);
             var listener_lexer = new ErrorListener<int>();
             var listener_parser = new ErrorListener<IToken>();
             lexer.AddErrorListener(listener_lexer);
             parser.AddErrorListener(listener_parser);
-            var tree = parser.TemplateStart();
+            var tree = parser.file();
             if (listener_lexer.had_error || listener_parser.had_error)
                 System.Console.WriteLine("error in parse.");
             else
