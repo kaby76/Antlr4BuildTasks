171155f5ac8557ccdb5782a668f2eca7562ad58a
Ken Domino <kenneth.domino@domemtech.com>
2021-02-19 08:36:14 -0500

    For JavaScript, replace unnecessary npm i code with just an npm install, since we already have a project.json file.

--- dotnet-antlr/Program.cs
+++ dotnet-antlr/Program.cs
@@ -693,21 +693,7 @@ run:
                 sb.AppendLine(@"#!/bin/sh
 rm -rf node_modules
 rm -f package-lock.json
-npm i antlr4@4.9.1
-if [[ ""$?"" != ""0"" ]]
-then
-    exit 1
-fi
-npm i typescript-string-operations@1.4.1
-if [[ ""$?"" != ""0"" ]]
-then
-    exit 1
-fi
-npm i fs-extra
-if [[ ""$?"" != ""0"" ]]
-then
-    exit 1
-fi
+npm install
 cp -r /c/Users/kenne/Documents/GitHub/antlr4/runtime/JavaScript/src/antlr4/* node_modules/antlr4/src/antlr4
 java -jar ~/Downloads/antlr4-4.9.2-SNAPSHOT-complete.jar -Dlanguage=JavaScript *.g4
 # java -jar ~/Downloads/antlr-4.9.1-complete.jar -Dlanguage=JavaScript *.g4
