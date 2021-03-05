304928d89361a85c77405bf3fa8242e4f8c06263
Ken Domino <kenneth.domino@domemtech.com>
2021-03-05 07:17:27 -0500

    Removing redirects, want to see everything.

--- dotnet-antlr/test.sh
+++ dotnet-antlr/test.sh
@@ -4,13 +4,13 @@ alias make='mingw32-make.exe'
 for i in CSharp Java JavaScript Dart Python3
 do
 	echo $i
-	rm -rf Generated > /dev/null 2>&1
-	dotnet-antlr -t $i > /dev/null 2>&1
-	pushd Generated > /dev/null 2>&1
-	ls > /dev/null 2>&1
-	mingw32-make.exe > /dev/null 2>&1
+	rm -rf Generated
+	dotnet-antlr -t $i
+	pushd Generated
+	ls
+	mingw32-make.exe
 	mingw32-make.exe run RUNARGS="-input 1+2 -tree"
-	mingw32-make.exe clean > /dev/null 2>&1
-	ls > /dev/null 2>&1
-	popd > /dev/null 2>&1
+	mingw32-make.exe clean
+	ls
+	popd
 done
