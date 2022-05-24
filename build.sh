a2d24a041657292c72fa002f6b78a91d70d71909
Ken Domino <kenneth.domino@domemtech.com>
2022-05-24 08:46:18 -0400

    Remove .jar. Release.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://repo1.maven.org/maven2/org/antlr/antlr4/4.10.1/antlr4-4.10.1-complete.jar
+# wget https://repo1.maven.org/maven2/org/antlr/antlr4/4.10.1/antlr4-4.10.1-complete.jar
 dotnet restore
 dotnet build
 unameOut="$(uname -s)"
