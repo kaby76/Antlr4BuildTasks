5d5ea72e8974abe4bf6e58a56c9b72bc18278515
Ken Domino <kenneth.domino@domemtech.com>
2022-09-23 16:23:15 -0400

    Update test for windows with corrected "dotnet add source" path--Windows is very picky. https://github.com/kaby76/Antlr4BuildTasks/issues/48

--- _tests/multitarget-clean/test-alone.sh
+++ _tests/multitarget-clean/test-alone.sh
@@ -26,12 +26,14 @@ cd "$where/../.."
 if [[ "$machine" == "MinGw" || "$machine" == "Msys" ]]
 then
     cwd=`pwd | sed 's%/c%c:%' | sed 's%/%\\\\%g'`
+    location="$cwd\\Antlr4BuildTasks\\bin\\Debug\\"
 else
     cwd=`pwd`
+    location="$cwd/Antlr4BuildTasks/bin/Debug/"
 fi
 echo "$machine"
 echo $cwd
-echo dotnet nuget add source "$cwd/Antlr4BuildTasks/bin/Debug/" --name nuget-antlr4buildtasks
-dotnet nuget add source "$cwd/Antlr4BuildTasks/bin/Debug/" --name nuget-antlr4buildtasks > /dev/null 2>&1
+echo dotnet nuget add source $location --name nuget-a4bt
+dotnet nuget add source $location --name nuget-a4bt > /dev/null 2>&1
 cd "$where"
 dotnet build
