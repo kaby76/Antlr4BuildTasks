a24314c402eaac415d648eaee3cef9022d0757bf
John Belcher <4042025+foo-queue@users.noreply.github.com>
2023-01-09 07:03:02 -0500

    Fix netstandard2.0 build errors

--- Antlr4BuildTasks/Tasks/MakeRelativeList.cs
+++ Antlr4BuildTasks/Tasks/MakeRelativeList.cs
@@ -1,19 +1,20 @@
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 using System;
+using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-using Directory = System.IO.Directory;
 
 namespace Antlr4.Build.Tasks
 {
     public class MakeRelativeList : Task
     {
-        private List<ITaskItem> _result = new List<ITaskItem>();
-        internal string WorkingDirectory { get; set; } = Directory.GetCurrentDirectory();		
         public ITaskItem[] List1 { get; set; }
+        private List<ITaskItem> _result = new List<ITaskItem>();
+        internal string WorkingDirectory { get; set; } = Directory.GetCurrentDirectory();
 
-        [Output] public ITaskItem[] Result
+        [Output]
+        public ITaskItem[] Result
         {
             get { return _result.ToArray(); }
             set { _result = new List<ITaskItem>(value); }
@@ -24,16 +25,17 @@ namespace Antlr4.Build.Tasks
             try
             {
                 _result.Clear();
-                if (List1 == null) 
+                if (List1 == null)
                     List1 = Array.Empty<ITaskItem>();
 
                 var slash = Path.DirectorySeparatorChar;
+                var slashStr = slash.ToString();
 
                 // Normalize workDir
                 var workDir = Path.GetFullPath(WorkingDirectory);
-                if (!workDir.EndsWith(slash)) workDir += slash;
+                if (!workDir.EndsWith(slashStr)) workDir += slash;
                 var workDirRoot = Path.GetPathRoot(workDir);
-                var workDirFolders = workDir.Substring(workDirRoot.Length).Split(slash, StringSplitOptions.RemoveEmptyEntries);
+                var workDirFolders = workDir.Substring(workDirRoot.Length).Split(new[] { slash }, StringSplitOptions.RemoveEmptyEntries);
 
                 this.Log.LogMessage("MakeRelativeList input: '{0}'", String.Join("', '", List1.AsEnumerable()));
                 this.Log.LogMessage("MakeRelativeList working directory: '{0}'", workDir);
@@ -65,7 +67,10 @@ namespace Antlr4.Build.Tasks
                             Environment.OSVersion.Platform == PlatformID.Unix
                             ? StringComparison.Ordinal
                             : StringComparison.OrdinalIgnoreCase;
-                        var comparer = StringComparer.FromComparison(comparison);
+                        var comparer =
+                            Environment.OSVersion.Platform == PlatformID.Unix
+                            ? StringComparer.Ordinal
+                            : StringComparer.OrdinalIgnoreCase;
 
                         // Normalize path
                         path = Path.GetFullPath(path);
@@ -90,12 +95,12 @@ namespace Antlr4.Build.Tasks
                         }
 
                         // 2) Ignore all leading directories they have in common (beware UNC paths)...
-                        var pathSegments = path.Substring(pathRoot.Length).Split(slash, StringSplitOptions.RemoveEmptyEntries);
+                        var pathSegments = path.Substring(pathRoot.Length).Split(new[] { slash }, StringSplitOptions.RemoveEmptyEntries);
 
-                        var numCommon = 
+                        var numCommon =
                             Enumerable
-                                .Zip(workDirFolders, pathSegments)
-                                .TakeWhile(t => comparer.Equals(t.First, t.Second))
+                                .Zip(workDirFolders, pathSegments, Tuple.Create)
+                                .TakeWhile(t => comparer.Equals(t.Item1, t.Item2))
                                 .Count();
 
                         var remainingWorkDirs = workDirFolders.Skip(numCommon);
@@ -103,9 +108,9 @@ namespace Antlr4.Build.Tasks
 
                         // 2) What remains in `remainingWorkDirs` is the number of ".."s 
                         //    needed to get to the base of `remainingPathSegments`...
-                        var relative_path = String.Join(slash, remainingWorkDirs.Select(_ => "..").Concat(remainingPathSegments));
+                        var relative_path = String.Join(slashStr, remainingWorkDirs.Select(_ => "..").Concat(remainingPathSegments));
                         if (relative_path == "") relative_path = ".";
-                        if (path.EndsWith(slash)) relative_path += slash;
+                        if (path.EndsWith(slashStr)) relative_path += slash;
                         _result.Add(new TaskItem(relative_path));
                     }
                     catch (Exception e)
